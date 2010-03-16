6776e9a6836f0664d12a2ed32f83b0c8c5775127
w89477 <w89477>
2010-03-16 03:23:51 +0000

    default parameter

---
+++ src/javascript/devicelocation/device-invariant/locate_defaultParam.js
@@ -0,0 +1,8 @@
+_DEVICELOCATION_WM_ = {};
+
+_DEVICELOCATION_WM_.defaultProximity = 5;
+_DEVICELOCATION_WM_.defaultTimeout = 10000;
+
+_DEVICELOCATION_WM_.defaultPreLocating = null;
+_DEVICELOCATION_WM_.defaultProcessLocation = null;
+_DEVICELOCATION_WM_.defaultProcessError = null;
