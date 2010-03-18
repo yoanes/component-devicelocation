6712cd554432d0ec3f680e69c6247ba3467d051d
w89477 <w89477>
2010-03-18 23:26:25 +0000

    rename locate_ to location_

---
+++ src/javascript/devicelocation/device-invariant/location_defaultParam.js
@@ -0,0 +1,9 @@
+_DEVICELOCATION_WM_ = {};
+
+_DEVICELOCATION_WM_.defaultOption = {'proximity': 5, 'timeout' : 10000};
+
+_DEVICELOCATION_WM_.defaultPreLocating = null;
+
+_DEVICELOCATION_WM_.defaultProcessLocation = null;
+
+_DEVICELOCATION_WM_.defaultProcessError = null;
