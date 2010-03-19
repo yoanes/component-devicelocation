245ee747b9da1e8772bdaa995b727ece410192aa
w89477 <w89477>
2010-03-19 04:58:04 +0000

    remove defaultParam.js

--- src/javascript/devicelocation/device-invariant/location_defaultParam.js
+++
@@ -1,9 +0,0 @@
-_DEVICELOCATION_WM_ = {};
-
-_DEVICELOCATION_WM_.defaultOption = {'proximity': 5, 'timeout' : 10000};
-
-_DEVICELOCATION_WM_.defaultPreLocating = null;
-
-_DEVICELOCATION_WM_.defaultProcessLocation = null;
-
-_DEVICELOCATION_WM_.defaultProcessError = null;
