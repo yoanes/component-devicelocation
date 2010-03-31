d119254a9a32798968ac9c23435325d0eb565c8e
w89477 <w89477>
2010-03-31 04:53:11 +0000

    processLocation() is now the default onlocate properties for autoLocate object. standardised the devloc option for timeout, maxAge, and highAcc

--- src/javascript/devicelocation/device-variant/iphone/location_defaultParam.js
+++ src/javascript/devicelocation/device-variant/iphone/location_defaultParam.js
@@ -1,6 +1,14 @@
 _DEVICELOCATION_WM_ = {};
 
-_DEVICELOCATION_WM_.processLocation = function(position) {
+_DEVICELOCATION_WM_.Locate = {};
+_DEVICELOCATION_WM_.Locate.onlocate = null;
+_DEVICELOCATION_WM_.Locate.onerror = null;
+_DEVICELOCATION_WM_.Locate.onprelocate= null;
+
+_DEVICELOCATION_WM_.marker = null;
+
+_DEVICELOCATION_WM_.AutoLocate = {};
+_DEVICELOCATION_WM_.AutoLocate.onlocate = function(position) {
 	if(position == null) return;
 	
 	/* grab the user location lat lon and convert it to that of EMS' */
@@ -18,26 +26,43 @@ _DEVICELOCATION_WM_.processLocation = function(position) {
 	if(MAP.instances[0].Map.getZoom() < 9)
 		setTimeout(function() {EMS.Util.smoothZoom(MAP.instances[0].Map, MAP.instances[0].Map.getCenter(), userLatLon, 11 );} , 500);
 	/* alternate version */
-	/*	MAP.instances[0].Map.zoomTo.delay(500, this, new Array(11)); */	
+	/*	MAP.instances[0].Map.zoomTo.delay(500, this, new Array(11)); */
 	
-	MAP.instances[0].Geocoder.reverseGeocode(userLatLon.longitude, userLatLon.latitude, function(addresses) {
+	MAP.instances[0].Geocoder.reverseGeocode(position.longitude, position.latitude, function(addresses) {
 		if(addresses.results.length>0){
 			var address = addresses.results[0]; 
 			var displayAddress = "";
-			displayAddress += address.streetNumber + " " + address.street.fullName + ", " + address.suburb + ", " + address.state;
+			displayAddress += address.streetNumber + " " + address.street.fullName.toLowerCase() + ", " + address.suburb.toLowerCase() + ", " + address.state;
 			$('currentAddress').innerHTML = displayAddress;
+			
+			if(_DEVICELOCATION_WM_.marker != null)
+				MAP.instances[0].Map.markersLayer.removeMarker(_DEVICELOCATION_WM_.marker);
+			
+			var locateMeIcon =  [{url: '/imageserver/common/images/component/map/markers/standard/dot_23x27.png',
+						  width: 23 , height: 27 , offsetX: -8 , offsetY: -27 , 
+						  coordinates: { latitude: position.latitude , longitude: position.longitude }
+						}];
+			
+			var locateMeMarker = {coordinates: locateMeIcon[0].coordinates};
+			
+			var locateMeIcon = {url: locateMeIcon[0].url, 
+					width: locateMeIcon[0].width,
+					height: locateMeIcon[0].height,
+					offsetX: locateMeIcon[0].offsetX,
+					offsetY: locateMeIcon[0].offsetY
+				};
+			
+			_DEVICELOCATION_WM_.marker = MAP.instances[0].addPoi(locateMeMarker, locateMeIcon);
 		}
 	});
 	
-	Reporting.to('http://'+window.location.host+'/?lat='+userLatLon.latitude+'&lon='+userLatLon.longitude);
+	Reporting.to('http://'+window.location.host+'/?dlat='+position.latitude+'&dlon='+position.longitude+'&dprox='+position.accuracy);
 };
 
-_DEVICELOCATION_WM_.Locate = {};
-_DEVICELOCATION_WM_.Locate.onlocate = null;
-_DEVICELOCATION_WM_.Locate.onerror = null;
-_DEVICELOCATION_WM_.Locate.onprelocate= null;
-
-_DEVICELOCATION_WM_.AutoLocate = {};
-_DEVICELOCATION_WM_.AutoLocate.onlocate = null;
 _DEVICELOCATION_WM_.AutoLocate.onerror = null;
 _DEVICELOCATION_WM_.AutoLocate.onprelocate= null;
+
+_DEVICELOCATION_WM_.options = {};
+_DEVICELOCATION_WM_.options.timeout = 2000;
+_DEVICELOCATION_WM_.options.maximumAge = 0;
+_DEVICELOCATION_WM_.options.enableHighAccuracy = true;
